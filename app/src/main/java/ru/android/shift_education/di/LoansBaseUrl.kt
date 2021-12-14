fb449db82d1314dd3b226c76847ba08780495a0c
Nik-Shalashov <monitor1@inbox.ru>
2021-12-14 19:44:36 +0700

    Модуль для Интернета и кастомная аннотация

---
+++ app/src/main/java/ru/android/shift_education/di/LoansBaseUrl.kt
@@ -0,0 +1,7 @@
+package ru.android.shift_education.di
+
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.RUNTIME)
+annotation class LoansBaseUrl
---
+++ app/src/main/java/ru/android/shift_education/di/NetworkModule.kt
@@ -0,0 +1,70 @@
+package ru.android.shift_education.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import ru.android.shift_education.data.network.AuthInterceptor
+import ru.android.shift_education.data.network.LoansApi
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class NetworkModule {
+
+    private companion object {
+        const val BASE_URL = "https://focusstart.appspot.com/"
+    }
+
+    @Provides
+    @LoansBaseUrl
+    fun provideBaseUrl(): String =
+        BASE_URL
+
+    @Provides
+    @Singleton
+    fun httpLoggingInterceptor(): HttpLoggingInterceptor =
+        HttpLoggingInterceptor().apply {
+            this.level = HttpLoggingInterceptor.Level.BODY
+        }
+
+    @Provides
+    @Singleton
+    fun provideClient(
+        authInterceptor: AuthInterceptor,
+        httpLoggingInterceptor: HttpLoggingInterceptor
+    ): OkHttpClient =
+        OkHttpClient.Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .addInterceptor(authInterceptor)
+            .build()
+
+    @Provides
+    fun provideGsonConverterFactory(): GsonConverterFactory =
+        GsonConverterFactory.create()
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(
+        gsonConverterFactory: GsonConverterFactory,
+        @LoansBaseUrl baseUrl: String,
+        client: OkHttpClient
+    ): Retrofit =
+        Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .client(client)
+            .addConverterFactory(gsonConverterFactory)
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideLoansApi(
+        retrofit: Retrofit
+    ): LoansApi =
+        retrofit.create(LoansApi::class.java)
+
+}
